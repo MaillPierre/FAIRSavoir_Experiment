68a4c4547920d3e61ab04eb55c3003fe04ca57f0
Pierre Maillot <maillpierre@gmail.com>
2023-10-12 11:06:38 +0200

    formatting for submission

--- dockerStart.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-sleep 20
-
-cd /indegx
-
-# Start the IndeGx app
-npm run run
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ ! -d "input/" ] && mkdir input/
-[ ! -d "output/" ] && mkdir output/
-
-containerName=`sed 's/\///g' < <(pwd)`
-containerName=`sed 's/.*/\L&/g' < <(echo "$containerName")`
-
-docker-compose -p $containerName up --abort-on-container-exit --build
